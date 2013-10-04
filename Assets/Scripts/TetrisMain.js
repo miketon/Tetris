cc58378a3b7732fe70f3ea3d59f21113c2b110c5
Mike Ton <mike.ton@gmail.com>
2013-10-03 19:01:19 -0700

    Modulus by num_xblocks vs magic number.

--- Assets/Scripts/TetrisMain.js
+++ Assets/Scripts/TetrisMain.js
@@ -6,11 +6,17 @@ private var tetromino : TetrisPieceControl ;
 private var xPos :int = 0;
 private var yPos :int = 0;
 
+private var xMax :int=1;
+private var yMax :int=1;
+
 function Start () {
 
   tGrid     = GetComponent(TetrisGrid)         ;
   tetromino = GetComponent(TetrisPieceControl) ;
   
+  xMax = tGrid.num_xblocks;
+  yMax = tGrid.num_yblocks;
+    
   xPos = 0;
   yPos = 2;
   tetromino.CreateNewPiece(tGrid, xPos, yPos, 1);
@@ -21,13 +27,23 @@ function Update () {
   if(Input.GetKeyDown(KeyCode.RightArrow)){
     print("Move Right");
     xPos += 1;
-    xPos = xPos%10;
+    xPos = (xPos)%(xMax);
     tetromino.CreateNewPiece(tGrid, xPos, yPos, 1);
     /*
     if(tetromino.PieceCanMove(tGrid, xPos, 0)){
       tetromino.MovePiece(xPos-1, 0);
     }
     */
+    tGrid.RenderGrid();
+  }
+  if(Input.GetKeyDown(KeyCode.LeftArrow)){
+    print("Move Left");
+    xPos -= 1;
+    if(xPos<0){
+      xPos = 0;
+    }
+    xPos = (xPos)%(xMax);
+    tetromino.CreateNewPiece(tGrid, xPos, yPos, 1);
     tGrid.RenderGrid();
   }
   else if(Input.GetKeyDown(KeyCode.U)){
