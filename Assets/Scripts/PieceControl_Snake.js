fe1f88dbc3950b6c52bbd888ebc9f6ec90b44f0a
Mike Ton <mike.ton@gmail.com>
2013-10-17 08:02:54 -0700

    Resolved Snake Tail Index.  Zero vs. One basis?

--- Assets/Scripts/PieceControl_Snake.js
+++ Assets/Scripts/PieceControl_Snake.js
@@ -3,20 +3,23 @@
 class PieceControl_Snake extends PieceControl_Tetris{
 
   protected var tailMax : int = 5   ;
-  protected var tailPos : Vector2[] = new Vector2[tailMax+1] ; //cursor/current position WTH?? HACK : Must define here, not in Start()???
+  private   var tailInd : int = 4   ; //HACK: max index = tailMax-1 ??? zero vs. one basis
+  protected var tailPos : Vector2[] ; //cursor/current position
   
   function Start () {
-    super.Start();
-    //tailMax=tGrid.num_xblocks;
+    super.Start()                  ;
+    tailMax = tGrid.num_xblocks    ;
+    tailPos = new Vector2[tailMax] ;
+    tailInd = tailMax-1            ; //HACK: max index = tailMax-1 ??? zero vs. one basis
   }
   
   function doMovePiece(){
     super.doMovePiece();
-    tailPos[tailMax] = pPos;            //add most recent tail block position
-    for(var i:int=0; i<tailMax; i++ ){  //shift tail array
+    for(var i:int=0; i<tailInd; i++ ){  //shift tail array
       tailPos[i] = tailPos[i+1];
     }
-    PieceRemove(tailPos[0]);            //drop oldest tail block position
+    tailPos[tailInd] = pPos ;           //add most recent tail block position
+    PieceRemove(tailPos[0]) ;           //drop oldest tail block position
   }
   
 }
