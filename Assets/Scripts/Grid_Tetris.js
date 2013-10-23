c7c1707724505f6db301262a44c015bede54bfc9
Mike Ton <mike.ton@gmail.com>
2013-10-22 19:02:53 -0700

    Separated RowDrop and RowClear functionality.

--- Assets/Scripts/Grid_Tetris.js
+++ Assets/Scripts/Grid_Tetris.js
@@ -5,43 +5,17 @@ class Grid_Tetris extends Grid{
   function CheckForCompletedRows(){
     print("Are there row complete?");
     for(var i:int=0; i<num_yblocks; i++){
-      isRowComplete(i);
+      RowCompleteCheck(i);
     }
   }
   
-  function isRowComplete(y:int) : boolean{
-    var completeBool : boolean = true;
-    for(var i:int=0; i<num_xblocks; i++){
-      if(GetBlockColor(Vector2(i,y)) == emptyBlock){
-        completeBool = false;
-        print("Found Empty block : Row Not Complete");
-      }
-    }
-    return completeBool;
-  }
-  
-  function DropRow(y:int){
+  function doDropRow(y:int){
     var rowAbove:int = y+1;
     if(rowAbove<num_yblocks){ //prevents dropping from the top of playfield
       for(var i:int=0; i<num_xblocks; i++){
         BlockDrop(Vector2(i,y));
       }
-      RowClear(rowAbove);
-    }
-  }
-  
-  function BlockDrop(vec2_IN:Vector2){
-    var blockColor:int = GetBlockColor(Vector2(vec2_IN.x, vec2_IN.y));
-    if( blockColor == emptyBlock){         //if current block is empty
-      var blockAbove:int = GetBlockColor(Vector2(vec2_IN.x, vec2_IN.y+1));
-      SetBlockColor(vec2_IN, blockAbove);  //set current block to block above's color
-      print("BlockDrop : "+blockColor+" : "+vec2_IN);
-    }
-  }
-  
-  function RowClear(y:int){
-    for(var i:int=0; i<num_xblocks; i++){
-      ClearBlock(Vector2(i,y));
+      RowDrop(rowAbove);
     }
   }
   
@@ -51,8 +25,40 @@ class Grid_Tetris extends Grid{
     }
   }
   
-  function Start(){
-    super.Start();
+  function RowClear(y:int){
+    for(var i:int=0; i<num_xblocks; i++){
+      ClearBlock(Vector2(i,y));
+    }
+  }
+  
+  function RowDrop(y:int){
+    for(var i:int=0; i<num_xblocks; i++){
+      if(GetBlockColor(Vector2(i,y)) == emptyBlock){
+        ClearBlock(Vector2(i,y));
+      }
+    }
+  }
+   
+  function RowCompleteCheck(y:int) : boolean{
+    var completeBool : boolean = true;
+    for(var i:int=0; i<num_xblocks; i++){
+      if(GetBlockColor(Vector2(i,y)) == emptyBlock){
+        completeBool = false;
+        print("Found Empty block : Row Not Complete");
+      }
+    }
+    return completeBool;
+  }
+   
+  function BlockDrop(vec2_IN:Vector2){
+    var abovePos  : Vector2 = Vector2(vec2_IN.x, vec2_IN.y+1);
+    var blockColor: int     = GetBlockColor(vec2_IN)         ;
+    if( blockColor == emptyBlock){         //if current block is empty
+      var aboveColor:int = GetBlockColor(abovePos);
+      SetBlockColor(vec2_IN, aboveColor) ;  //set current block to block above's color
+      SetBlockColor(abovePos, emptyBlock);  //set block above to empty
+      print("BlockDrop : "+aboveColor+" : "+vec2_IN);
+    }
   }
    
 }
--- Assets/Scripts/Main_Tetris.js
+++ Assets/Scripts/Main_Tetris.js
@@ -3,6 +3,7 @@
 class Main_Tetris extends MonoBehaviour{
 
 protected var tGrid     : Grid_Tetris         ;
+protected var tGrid_y   : int = 1             ;
 protected var tetromino : PieceControl_Tetris ;
 
 protected var kPos : Vector2 = Vector2(0, 0); //current pos
@@ -12,7 +13,8 @@ protected var kRt  : Vector2 = Vector2(1, 0);
 protected var kLt  : Vector2 = Vector2(-1,0);
 
 function Start () {
-  tGrid     = GetComponent(Grid_Tetris)          ;
+  tGrid     = GetComponent(Grid_Tetris)         ;
+  tGrid_y   = tGrid.num_yblocks                 ;
   tetromino = GetComponent(PieceControl_Tetris) ;
   
   kPos = Vector2(0,2);    
@@ -33,20 +35,24 @@ function Update () {
   }
   else if(Input.GetKeyDown(KeyCode.Space)){
     kPos = tetromino.GetPos() ;
-    tGrid.RowClear(kPos.y)    ;
-    tGrid.DropRow(kPos.y)     ;
-    //tetromino.PieceAdd(Vector2(kPos.x,kPos.y), 3);
+    for(var i:int=0; i<tGrid_y; i++){
+      tGrid.doDropRow(i) ;
+    }
   }
   else if(Input.GetKeyDown(KeyCode.F)){
     kPos = tetromino.GetPos() ;
     tGrid.RowFill(kPos.y,0)   ;
   }
   else if(Input.GetKeyDown(KeyCode.C)){
-    kPos = tetromino.GetPos();
+    kPos = tetromino.GetPos() ;
+    tGrid.RowClear(kPos.y);
+  /*
     var rowComplete:boolean = tGrid.isRowComplete(kPos.y);
     if(rowComplete){
       tGrid.RowFill(kPos.y,1);
     }
+    tGrid.RowFill(tGrid_y-1,0);
+  */
   }
 }
 
