40e56cefbf46488eaf733ef77a1712ae76cfa15d
Mike Ton <mike.ton@gmail.com>
2013-10-25 08:10:39 -0700

    Adding waitForAnim:int //0=false, 1=clear row, 2=drop row

--- Assets/Scripts/Main_Tetris.js
+++ Assets/Scripts/Main_Tetris.js
@@ -2,6 +2,8 @@
 
 class Main_Tetris extends MonoBehaviour{
 
+  private var waitForAnim : int = 0 ;  //0=false, 1=clear row, 2=drop row
+
   protected var tGrid     : Grid_Tetris         ;
   protected var tGrid_y   : int = 1             ;
   protected var tetromino : PieceControl_Tetris ;
@@ -21,29 +23,44 @@ class Main_Tetris extends MonoBehaviour{
   }
 
   function Update () {
-    if(Input.GetKeyDown(KeyCode.RightArrow)){
-      tetromino.MovePiece(kRt);
+    if(Input.GetKeyDown(KeyCode.W)){ //modulate through wait states
+      waitForAnim = waitForAnim+1;
+      waitForAnim = waitForAnim%3;
     }
-    else if(Input.GetKeyDown(KeyCode.LeftArrow)){
-      tetromino.MovePiece(kLt);
+
+    if(waitForAnim>0){       //does animation need to play; don't take input
+      if(waitForAnim == 1){  //1==clear row
+        print("Animation in process : Clearing Row " + waitForAnim);
+      }
+      else{                  //1==dropping row
+        print("Animation in process : Dropping Row " + waitForAnim);
+      }
     }
-    else if(Input.GetKeyDown(KeyCode.UpArrow)){
-      tetromino.MovePiece(kUp);
-    }
-    else if(Input.GetKeyDown(KeyCode.DownArrow)){
-      tetromino.MovePiece(kDn);
-    }
-    else if(Input.GetKeyDown(KeyCode.Space)){
-      doDrop();
-    }
-    else if(Input.GetKeyDown(KeyCode.F)){
-      kPos = tetromino.GetPos() ;
-      tGrid.RowFill(kPos.y,0)   ;
-    }
-    else if(Input.GetKeyDown(KeyCode.C)){
-      kPos = tetromino.GetPos() ;
-      tGrid.RowClear(kPos.y);
-      doDrop();
+    else{                    //else take input
+      if(Input.GetKeyDown(KeyCode.RightArrow)){
+        tetromino.MovePiece(kRt);
+      }
+      else if(Input.GetKeyDown(KeyCode.LeftArrow)){
+        tetromino.MovePiece(kLt);
+      }
+      else if(Input.GetKeyDown(KeyCode.UpArrow)){
+        tetromino.MovePiece(kUp);
+      }
+      else if(Input.GetKeyDown(KeyCode.DownArrow)){
+        tetromino.MovePiece(kDn);
+      }
+      else if(Input.GetKeyDown(KeyCode.Space)){
+        doDrop();
+      }
+      else if(Input.GetKeyDown(KeyCode.F)){
+        kPos = tetromino.GetPos() ;
+        tGrid.RowFill(kPos.y,0)   ;
+      }
+      else if(Input.GetKeyDown(KeyCode.C)){
+        kPos = tetromino.GetPos() ;
+        tGrid.RowClear(kPos.y);
+        doDrop();
+      }
     }
   }
 
