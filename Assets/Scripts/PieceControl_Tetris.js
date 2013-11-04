6ec8135d0a01325628e39c0aa8c6a6ee9e3b68a7
Mike Ton <mike.ton@gmail.com>
2013-11-04 08:02:25 -0800

    Silo Animation State from Delta To Zero function

--- Assets/Scripts/PieceControl_Tetris.js
+++ Assets/Scripts/PieceControl_Tetris.js
@@ -23,33 +23,45 @@ class PieceControl_Tetris extends MonoBehaviour{
     if(Input.GetKeyDown(KeyCode.W)){ //modulate through wait states
       inputB = !inputB;
       if(!inputB){
-        doAnimPiece() ;
-        kZero = 1.0   ;
+        mBlockGet() ;
+        kOffS = 1.0   ;
       }
     }
     else if(Input.GetKeyDown(KeyCode.Y)){ //modulate through wait states
-      doAnimPiece();
+      mBlockGet();
     }
     
     if(bAnim){
-      kZero = deltaToZero(kZero, 0.15);
-      var index_B   : int       = tGrid._Pos_To_Index(cPos)   ;
-      var animBlock : Transform = tGrid.blocks_xform[index_B] ;
-      animBlock.position.x = offSet.x+1.0-kZero;
-      print("kZero : "+kZero +" block Position "+animBlock.position.x+" offSetX "+offSet.x);
+      mBlockAnim(1.0);
+      //print("kOffS : "+kOffS +" block Position "+mBlock.position.x+" offSetX "+offSet.x);
     }    
 
   }
   
-  var kZero : float   = 1.0   ;
-  var bAnim : boolean = false ;
+  var mBlock : Transform ; //current block getting moved
   var offSet : Vector2 = Vector2(0.0, 0.0);
+  var bAnim  : boolean = false ; //animating?
+  var kOffS  : float   = 1.0   ; //change in offset
+  var kAnim  : float   = 0.15  ; //interval change between frame step
+  
+  function getBlockXform(vec2_IN:Vector2):Transform{
+    var index_B : int = tGrid._Pos_To_Index(vec2_IN)   ;
+    return tGrid.blocks_xform[index_B] ;
+  }
     
-  function doAnimPiece(){
-    var index_B   : int       = tGrid._Pos_To_Index(cPos)   ;
-    var animBlock : Transform = tGrid.blocks_xform[index_B] ;
-    bAnim = true;
-    offSet.x = animBlock.position.x;
+  function mBlockGet(){
+    bAnim    = true;
+    mBlock   = getBlockXform(cPos);
+    offSet.x = mBlock.position.x;
+  }
+  
+  function mBlockAnim(distance:float){
+    kOffS = deltaToZero(kOffS, 0.15);
+    mBlock.position.x = offSet.x + distance -kOffS;
+    if(kOffS==0.0){
+      bAnim    = false ;
+      kOffS    = 1.0   ;
+    }
   }
   
   function deltaToZero(value:float, k:float):float{
@@ -59,8 +71,6 @@ class PieceControl_Tetris extends MonoBehaviour{
     }
     else{
       retValue = 0.0   ;
-      bAnim    = false ;
-      kZero    = 1.0   ;
     }
     return retValue ;
   }
